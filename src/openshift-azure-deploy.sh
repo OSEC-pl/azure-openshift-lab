ce80c0a9866468ddce33b1eabfd32e49d230972a
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2018-08-13 14:26:13 +0200

    Make this hopefully work finally on aŻur.

--- src/openshift-azure-deploy.sh
+++ src/openshift-azure-deploy.sh
@@ -6,14 +6,13 @@
 AZ_LOCATION=eastus
 AZ_RG_OPENSHIFT=openshiftrg
 AZ_RG_KEYVAULT=keyvaultrg
-AZ_KEYVAULT=ae119606f058407bb916
+AZ_KEYVAULT=ae119606f058407bb911
 AZ_SSH_PRIVKEY=~/.ssh/azure_openshift_rsa
 AZ_SSH_SECRET=keysecret
 AZ_SP=openshiftsp
 AZ_GROUP_OPENSHIFT=myOpenShiftCluster
 AZ_OPENSHIFT_TEMPLATE=https://raw.githubusercontent.com/Microsoft/openshift-origin/master/azuredeploy.json
-
-#AZ_SP_PASS=
+AZ_OPENSHIFT_ADMIN=clusteradmin
 
 check_binary() {
 	which $1 &>/dev/null
@@ -53,8 +52,19 @@ az_provision() {
 	fi
 }
 
+az_provision_sp() {
+	az ad sp create-for-rbac --name ${AZ_SP} \
+	          --role Contributor --password ${AZ_SP_PASS} \
+	          --scopes $(az group show --name ${AZ_RG_OPENSHIFT} --query id)
+}
+
 check_binary az
 check_binary jq
+
+if [ -z "$AZ_SP_PASS" ] ; then
+	echo Please set $AZ_SP_PASS env variable.
+	exit 1
+fi
 
 az_account_state_check
 
@@ -70,17 +80,45 @@ if [ ! -f ${AZ_SSH_PRIVKEY} ] ; then
 fi
 
 az keyvault secret set --vault-name ${AZ_KEYVAULT} --name ${AZ_SSH_SECRET} --file ${AZ_SSH_PRIVKEY}
+sleep 1
 
-# get appId from this:
-#az ad sp create-for-rbac --name ${AZ_SP} \
-#          --role Contributor --password ${AZ_SP_PASS} \
-#          --scopes $(az group show --name ${AZ_RG_OPENSHIFT} --query id)
+appid=$(az ad sp show --id http://${AZ_SP} | jq -r .appId)
 
-#az group deployment create -g ${AZ_RG_OPENSHIFT} --name ${AZ_GROUP_OPENSHIFT} \
-#      --template-uri ${AZ_OPENSHIFT_TEMPLATE}
-#      --parameters @./azuredeploy.parameters.json
+if [ -z "$appid" ] ; then
+	az_provision_sp
+	appid=$(az ad sp show --id http://${AZ_SP} | jq -r .appId)
 
-#  ssh -p 2200 clusteradmin@myopenshiftmaster.cloudapp.azure.com
+	if [ -z "$appid" ] ; then
+		echo Problem creating service principal
+		exit 20
+	fi
+fi
 
-# az group delete --name myResourceGroup
+echo Using appId: ${appid}
+
+pubkey=$(cat ${AZ_SSH_PRIVKEY}.pub | sed "s/ /\\\ /g")
+
+az group deployment create -g ${AZ_RG_OPENSHIFT} --name ${AZ_GROUP_OPENSHIFT} \
+      --template-uri ${AZ_OPENSHIFT_TEMPLATE} \
+      --parameters @./azuredeploy.parameters.json
+	masterVmSize=Standard_E2s_v3 \
+	infraVmSize=Standard_E2s_v3 \
+	nodeVmSize=Standard_E2s_v3 \
+	openshiftClusterPrefix=mycluster \
+	masterInstanceCount=3 \
+	infraInstanceCount=2 \
+	nodeInstanceCount=2 \
+	dataDiskSize=128 \
+	adminUsername=${AZ_OPENSHIFT_ADMIN} \
+	openshiftPassword=${AZ_SP_PASS} \
+#	sshPublicKey=${pubkey}  \
+	keyVaultResourceGroup=${AZ_RG_KEYVAULT} \
+	keyVaultName=${AZ_KEYVAULT} \ 
+	keyVaultSecret=${AZ_SSH_SECRET} \
+	aadClientId=$appid \
+	aadClientSecret=${AZ_SP_PASS} \
+	defaultSubDomainType=nipio \
+#	defaultSubDomain="openshiftlab.osec.pl"
+
+#echo ssh -p 2200 ${AZ_OPENSHIFT_ADMIN}@myopenshiftmaster.cloudapp.azure.com
 
